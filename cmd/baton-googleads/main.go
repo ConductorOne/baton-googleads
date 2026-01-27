95f919cd9c7a75e40e1639f7c3494a0edbc06ae7
Steve Gontzes <gontzess@gmail.com>
2026-01-27 15:21:27 -0500

    Upgrade baton-sdk to v0.7.4 and Go to 1.25.2

--- cmd/baton-googleads/main.go
+++ cmd/baton-googleads/main.go
@@ -5,12 +5,15 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/conductorone/baton-sdk/pkg/cli"
+	"github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
+	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
+	"github.com/spf13/viper"
 	"go.uber.org/zap"
 
+	googleadsconfig "github.com/conductorone/baton-googleads/pkg/config"
 	"github.com/conductorone/baton-googleads/pkg/connector"
 )
 
@@ -19,15 +22,19 @@ var version = "dev"
 func main() {
 	ctx := context.Background()
 
-	cfg := &config{}
-	cmd, err := cli.NewCmd(ctx, "baton-googleads", cfg, validateConfig, getConnector)
+	_, cmd, err := config.DefineConfiguration(
+		ctx,
+		"baton-googleads",
+		getConnector,
+		googleadsconfig.ConfigurationSchema,
+		connectorrunner.WithDefaultCapabilitiesConnectorBuilder(&connector.Connector{}),
+	)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
 
 	cmd.Version = version
-	cmdFlags(cmd)
 
 	err = cmd.Execute()
 	if err != nil {
@@ -36,10 +43,14 @@ func main() {
 	}
 }
 
-func getConnector(ctx context.Context, cfg *config) (types.ConnectorServer, error) {
+func getConnector(ctx context.Context, v *viper.Viper) (types.ConnectorServer, error) {
 	l := ctxzap.Extract(ctx)
 
-	cb, err := connector.New(ctx, cfg.CredentialsJSON, cfg.DeveloperToken, cfg.CustomerID)
+	credentialsJSON := v.GetString(googleadsconfig.CredentialsJSONField.FieldName)
+	developerToken := v.GetString(googleadsconfig.DeveloperTokenField.FieldName)
+	customerID := v.GetString(googleadsconfig.CustomerIDField.FieldName)
+
+	cb, err := connector.New(ctx, credentialsJSON, developerToken, customerID)
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
 		return nil, err
---
+++ pkg/config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"github.com/conductorone/baton-sdk/pkg/field"
+)
+
+var (
+	CredentialsJSONField = field.StringField(
+		"credentials-json-file-path",
+		field.WithDescription("JSON credentials file path for the Google ads account credentials"),
+		field.WithRequired(true),
+	)
+
+	DeveloperTokenField = field.StringField(
+		"developer-token",
+		field.WithDescription("Your google ads developer token"),
+		field.WithRequired(true),
+	)
+
+	CustomerIDField = field.StringField(
+		"customer-id",
+		field.WithDescription("If you are using a manager account to access a client account, you must provide the correct login customer ID"),
+	)
+
+	ConfigurationFields = []field.SchemaField{
+		CredentialsJSONField,
+		DeveloperTokenField,
+		CustomerIDField,
+	}
+
+	ConfigurationSchema = field.Configuration{
+		Fields: ConfigurationFields,
+	}
+)
+
+//go:generate go run ./gen
+var Config = field.NewConfiguration(ConfigurationFields)
